c9cc37cb0d9afe366febc4c61cc42fadac5df8c3
IACTU <john@jbdm.org>
2025-02-14 13:09:07 -0600

    Translated mixins from yarn to official

--- src/main/java/com/dimaskama/orthocamera/mixin/GameRendererMixin.java
+++ src/main/java/com/dimaskama/orthocamera/mixin/GameRendererMixin.java
@@ -4,10 +4,10 @@ import com.dimaskama.orthocamera.client.OrthoCamera;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.systems.VertexSorter;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.util.math.MathHelper;
+import com.mojang.blaze3d.vertex.VertexSorting;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.util.Mth;
 import org.joml.Matrix4f;
 import org.joml.Quaternionf;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,10 +17,10 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(GameRenderer.class)
 abstract class GameRendererMixin {
     @ModifyArg(
-            method = "renderWorld",
+            method = "renderLevel",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/WorldRenderer;setupFrustum(Lnet/minecraft/util/math/Vec3d;Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;)V"
+                    target = "Lnet/minecraft/client/renderer/LevelRenderer;prepareCullFrustum(Lnet/minecraft/world/phys/Vec3;Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;)V"
             ),
             index = 2
     )
@@ -32,36 +32,35 @@ abstract class GameRendererMixin {
     }
 
     @ModifyArg(
-            method = "renderWorld",
+            method = "renderLevel",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/WorldRenderer;render(Lnet/minecraft/client/render/RenderTickCounter;ZLnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/GameRenderer;Lnet/minecraft/client/render/LightmapTextureManager;Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;)V"
-
+                    target = "Lnet/minecraft/client/renderer/LevelRenderer;renderLevel(Lnet/minecraft/client/DeltaTracker;ZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;)V"
             ),
             index = 6
     )
-    private Matrix4f orthoProjMat(Matrix4f projMat, @Local(argsOnly = true) RenderTickCounter tickCounter) {
+    private Matrix4f orthoProjMat(Matrix4f projMat, @Local(argsOnly = true) DeltaTracker tickCounter) {
         if (OrthoCamera.isEnabled()) {
-            Matrix4f mat = OrthoCamera.createOrthoMatrix(tickCounter.getTickDelta(false), 0.0F);
-            RenderSystem.setProjectionMatrix(mat, VertexSorter.BY_Z);
+            Matrix4f mat = OrthoCamera.createOrthoMatrix(tickCounter.getGameTimeDeltaPartialTick(false), 0.0F);
+            RenderSystem.setProjectionMatrix(mat, VertexSorting.ORTHOGRAPHIC_Z);
             return mat;
         }
         return projMat;
     }
 
     @ModifyExpressionValue(
-            method = "renderWorld",
+            method = "renderLevel",
             at = @At(
                     value = "INVOKE",
                     target = "Lorg/joml/Quaternionf;conjugate(Lorg/joml/Quaternionf;)Lorg/joml/Quaternionf;"
             )
     )
-    private Quaternionf modifyRotation(Quaternionf original, @Local(argsOnly = true) RenderTickCounter tickCounter) {
+    private Quaternionf modifyRotation(Quaternionf original, @Local(argsOnly = true) DeltaTracker tickCounter) {
         if (OrthoCamera.isEnabled() && OrthoCamera.CONFIG.fixed) {
-            float delta = tickCounter.getTickDelta(false);
+            float delta = tickCounter.getGameTimeDeltaPartialTick(false);
             return original.rotationXYZ(
-                    OrthoCamera.CONFIG.getFixedPitch(delta) * MathHelper.RADIANS_PER_DEGREE,
-                    OrthoCamera.CONFIG.getFixedYaw(delta) * MathHelper.RADIANS_PER_DEGREE - MathHelper.PI,
+                    OrthoCamera.CONFIG.getFixedPitch(delta) * Mth.DEG_TO_RAD,
+                    OrthoCamera.CONFIG.getFixedYaw(delta) * Mth.DEG_TO_RAD - Mth.PI,
                     0.0F
             );
         }
